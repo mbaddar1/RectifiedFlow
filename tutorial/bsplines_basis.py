854fbe3e8d8406e136b689711bb18affbbf73d69
mbaddar <mbaddar2@gmail.com>
2024-07-24 20:27:02 +0300

    add unit test to b splines code

--- tutorial/bsplines_basis.py
+++ tutorial/bsplines_basis.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 import torch
 from bspline import splinelab, bspline
@@ -19,6 +21,20 @@ Memoization in Python
     * https://wiingy.com/learn/python/memoization-using-decorators-in-python/ 
     * https://www.geeksforgeeks.org/memoization-using-decorators-in-python/ 
 """
+
+
+def B(x, k, i, t):
+    if k == 0:
+        return 1.0 if t[i] <= x < t[i + 1] else 0.0
+    if t[i + k] == t[i]:
+        c1 = 0.0
+    else:
+        c1 = (x - t[i]) / (t[i + k] - t[i]) * B(x, k - 1, i, t)
+    if t[i + k + 1] == t[i + 1]:
+        c2 = 0.0
+    else:
+        c2 = (t[i + k + 1] - x) / (t[i + k + 1] - t[i + 1]) * B(x, k - 1, i + 1, t)
+    return c1 + c2
 
 
 class BSplinesBasis:
@@ -66,7 +82,7 @@ def bsplines_basis_v1():
 
     knots = np.linspace(0, 1, nknots)  # create a knot vector without endpoint repeats
     k = splinelab.augknt(knots, p)  # add endpoint repeats as appropriate for spline order p
-    B = bspline.Bspline(k, 2)  # create a spline basis of order p on knots k
+    B = bspline.Bspline(k, p)  # create a spline basis of order p on knots k
 
     A0 = B.collmat(tau)  # collocation matrix for function value at sites tau
     x = k
@@ -76,44 +92,71 @@ def bsplines_basis_v1():
     print("Finished")
 
 
-if __name__ == '__main__':
-    # bsplines_basis_v1()
-    n_knots = 100
+def bsplines_unit_test():
+    """
+        This function is to test my Bsplines implementation vs a naive Bsplines function
+        https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.BSpline.html
+
+        However, the values are not the same with bsplines method here
+        https://github.com/johntfoster/bspline
+    """
+    degree = 2
+    nknots = 10
     u_low = 0.0
     u_high = 1.0
-    bsp = BSplinesBasis(u_low=u_low, u_high=u_high, n_knots=n_knots, degree=2)
-    # bsp.N(u=0.1, i=1, p=2)
-    # print(bsp.num_memoized_calls)
-    u_arr = np.linspace(start=0.0, stop=1.0, num=50 + 1)
-    for i in range(n_knots):
-        bsp_arr = []
-        for u in u_arr:
-            b = bsp.calculate_N(u, i)
-            bsp_arr.append(b)
-        plt.plot(u_arr, bsp_arr)
-    print(f"all_calls = {bsp.all_calls}")
-    print(f"memoized_calls = {bsp.num_memoized_calls}")
-    print(f"fraction = {float(bsp.num_memoized_calls) / float(bsp.all_calls)}")
-    plt.savefig("bsplines.png")
-    print("finished")
-    # bsp.N(u=0.1, i=0, p=1)
-    # d = 2
-    # degrees = [5] * d
-    # domain = [[-1.0, 1.0] for _ in range(d)]
-    # op = orthpoly(degrees, domain)
-    # x = torch.tensor([-2, 0]).view(1, -1)
-    # feat = op(x)
-    # print("Finished")
-    # # knots = np.arange(-1, 1.001, 0.1)
-    # # c = np.array([-1, 2, 0, -1])
-    # # k = 2
-    # # bspl = BSpline.basis_element(t=knots)
-    # # spl_ = bspl(0.1)
-    # #
-    # # print(spl_)
-    # # import numpy as np
-    # # from scipy.interpolate import BSpline
-    # #
-    # # b = BSpline.basis_element([0, 1, 2, 3, 4])
-    # # u = b(2)
-    # # print("finished")q
+    bsp2 = BSplinesBasis(u_low=u_low, u_high=u_high, n_knots=nknots, degree=degree)
+    for i in range(0, nknots):
+        for x in np.arange(u_low, u_high, 0.01):
+            bval = bsp2.calculate_N(u=x, i=i)
+            bval2 = B(x=x, k=degree, i=i, t=bsp2.u_) # pass the augmented knots
+            assert np.abs(bval - bval2) < 1e-6, f"N(x={x},i={i},p={degree}) are not equal : {bval}!={bval2}"
+    print("Finished Unit Test")
+
+
+if __name__ == '__main__':
+    # bsplines_basis_v1()
+    bsplines_unit_test()
+
+    # TODO - to delete
+    ### Sandbox Code ###
+    # sys.exit(-1)
+    # n_knots = 100
+    # u_low = 0.0
+    # u_high = 1.0
+    # bsp1 = ""
+    # bsp2 = BSplinesBasis(u_low=u_low, u_high=u_high, n_knots=n_knots, degree=2)
+    # # bsp.N(u=0.1, i=1, p=2)
+    # # print(bsp.num_memoized_calls)
+    # u_arr = np.linspace(start=0.0, stop=1.0, num=50 + 1)
+    # for i in range(n_knots):
+    #     bsp_arr = []
+    #     for u in u_arr:
+    #         b = bsp2.calculate_N(u, i)
+    #         bsp_arr.append(b)
+    #     plt.plot(u_arr, bsp_arr)
+    # print(f"all_calls = {bsp2.all_calls}")
+    # print(f"memoized_calls = {bsp2.num_memoized_calls}")
+    # print(f"fraction = {float(bsp2.num_memoized_calls) / float(bsp2.all_calls)}")
+    # plt.savefig("bsplines.png")
+    # print("finished")
+    # # bsp.N(u=0.1, i=0, p=1)
+    # # d = 2
+    # # degrees = [5] * d
+    # # domain = [[-1.0, 1.0] for _ in range(d)]
+    # # op = orthpoly(degrees, domain)
+    # # x = torch.tensor([-2, 0]).view(1, -1)
+    # # feat = op(x)
+    # # print("Finished")
+    # # # knots = np.arange(-1, 1.001, 0.1)
+    # # # c = np.array([-1, 2, 0, -1])
+    # # # k = 2
+    # # # bspl = BSpline.basis_element(t=knots)
+    # # # spl_ = bspl(0.1)
+    # # #
+    # # # print(spl_)
+    # # # import numpy as np
+    # # # from scipy.interpolate import BSpline
+    # # #
+    # # # b = BSpline.basis_element([0, 1, 2, 3, 4])
+    # # # u = b(2)
+    # # # print("finished")q
